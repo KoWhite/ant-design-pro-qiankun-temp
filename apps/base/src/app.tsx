24b28613e8d2ec6fc161f3810b5372fe59aff2fb
MichLiu <602502634@qq.com>
2024-11-05 13:46:44 +0800

    fix: test

--- apps/base/src/app.tsx
+++ apps/base/src/app.tsx
@@ -3,7 +3,7 @@ import { LinkOutlined } from '@ant-design/icons';
 import type { Settings as LayoutSettings } from '@ant-design/pro-components';
 import { SettingDrawer } from '@ant-design/pro-components';
 import type { RunTimeLayoutConfig } from '@umijs/max';
-import { history, Link } from '@umijs/max';
+import { history, Link, RequestConfig } from '@umijs/max';
 import defaultSettings from '../config/defaultSettings';
 import { errorConfig } from './requestErrorConfig';
 import { currentUser as queryCurrentUser } from '@/services/ant-design-pro/api';
@@ -131,7 +131,9 @@ export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) =
  * 它基于 axios 和 ahooks 的 useRequest 提供了一套统一的网络请求和错误处理方案。
  * @doc https://umijs.org/docs/max/request#配置
  */
-export const request = {
+console.log('process.env.API_URL', process.env);
+export const request: RequestConfig = {
+  baseURL: process.env.API_URL,
   ...errorConfig,
 };
 
